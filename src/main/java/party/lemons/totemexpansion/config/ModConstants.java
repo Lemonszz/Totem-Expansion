580b88a1175905ace04a50087e263439c0b4937c
Lemonszz <lemonverse@gmail.com>
2018-04-08 07:59:17 +1200

    version bump

--- src/main/java/party/lemons/totemexpansion/config/ModConstants.java
+++ src/main/java/party/lemons/totemexpansion/config/ModConstants.java
@@ -7,5 +7,5 @@ public class ModConstants
 {
 	public static final String MODID = "totemexpansion";
 	public static final String NAME = "Totem Expansion";
-	public static final String VERSION = "1.0.4";
+	public static final String VERSION = "1.0.5";
 }
